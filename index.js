3713233e89d01e036b15d93d2aaf896db756a963
NonoPir <sanoonas100@gmail.com>
2023-04-01 22:16:58 +0400

    Added new section "My Projects"

--- index.js
+++ index.js
@@ -37,3 +37,20 @@ document.querySelector(".mode2").addEventListener("click", function () {
   document.querySelector(".uil2").style.color = "#babddb";
   document.querySelector(".uil").style.color = "#babddb";
 });
+
+const productContainers = [...document.querySelectorAll(".product-container")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
+
+productContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
